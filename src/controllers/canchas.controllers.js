2aab849b7dd671e81b23d1d9add06ab0bf10b036
jonathan <jonathanfiorenza6@gmail.com>
2026-02-11 17:24:09 -0300

    implementa la función listarCanchas para obtener reservas desde la base de datos

--- src/controllers/canchas.controllers.js
+++ src/controllers/canchas.controllers.js
@@ -1,6 +1,11 @@
-export const listarCanchas = (req, res) => {
-  console.log("Prueba Ruta de canchas");
-  res.send("✅ Ruta de canchas funcionando!");
+export const listarCanchas = async (req, res) => {
+  try {
+    const reservas = await Reserva.find();
+    res.status(200).json(reservas);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar listar las reservas" });
+  }
 };
-
-
