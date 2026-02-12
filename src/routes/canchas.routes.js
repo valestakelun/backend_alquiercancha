4e18b0c89d0f933695df0e98c7af5950d3b87f6a
Corina <corinajimenez061102@gmail.com>
2026-02-12 09:22:44 -0300

    Agregamos la ruta de editar Reserva

--- src/routes/canchas.routes.js
+++ src/routes/canchas.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { crearReserva, listarCanchas } from "../controllers/canchas.controllers.js";
+import { crearReserva, listarCanchas, editarReserva } from "../controllers/canchas.controllers.js";
 
 const router = Router();
 
 router.route("/").post(crearReserva).get(listarCanchas)
+router.route("/:id").put(editarReserva)
 export default router;
