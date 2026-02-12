c36c3c9915919cc5d841da2025193a84601166ed
jonathan <jonathanfiorenza6@gmail.com>
2026-02-11 22:59:01 -0300

    agregar función para obtener cancha por ID

--- src/controllers/canchas.controllers.js
+++ src/controllers/canchas.controllers.js
@@ -25,3 +25,19 @@ export const listarCanchas = async (req, res) => {
       .json({ mensaje: "Ocurrio un error al intentar listar las reservas" });
   }
 };
+ //buscar canchas por id
+export const obtenerCanchaPorId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const reserva = await Reserva.findById(id);
+    if (!reserva) {
+      return res.status(404).json({ mensaje: "La reserva no existe" });
+    } else {
+      res.status(200).json(reserva);
+    }         
+  } catch (error) {
+    console.error(error);
+    res     .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar listar la reserva" });
+  }
+};        
