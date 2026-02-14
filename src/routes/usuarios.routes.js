5e5f4021046e542b5cc2187d85fdbfb641078d8b
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-02-14 08:58:18 -0300

    chore: se creó estructura base de middlewares para validaciones

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { login } from "../controllers/usuarios.controllers.js";
-
+import { validacionesLogin } from "../middlewares/validacionesLogin.js";
+import { validarCampos } from "../middlewares/validarCampos.js";
 const router = Router();
 
 // ✅ POST /api/usuarios/login
