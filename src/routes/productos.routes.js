d3e0a966a3b3824cc5f2e87d67261e8d9778269d
jonathan <jonathanfiorenza6@gmail.com>
2026-02-15 23:33:08 -0300

    agregue middleware errorMulter para manejar errores de tamaño de archivo en la subida de imágenes

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   crearProducto
 } from "../controllers/productos.controlles.js";
+import upload from "../helpers/upload.js"; 
+import errorMulter from "../middlewares/errorMulter.js";
 
 
 const router = Router();
 
-router.post("/", crearProducto);
+router.post("/", upload.single("imagen"), errorMulter, crearProducto);
 
 
 export default router;
