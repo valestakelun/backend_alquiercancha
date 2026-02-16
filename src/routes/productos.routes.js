b8714cd365450d5cfccdf09b48a0e3507941dd8d
jonathan <jonathanfiorenza6@gmail.com>
2026-02-15 23:45:15 -0300

    mejore  la ruta para la creación de productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,15 +1,10 @@
 import { Router } from "express";
-import {
-  crearProducto
-} from "../controllers/productos.controlles.js";
-import upload from "../helpers/upload.js"; 
+import { crearProducto } from "../controllers/productos.controlles.js";
+import upload from "../helpers/upload.js";
 import errorMulter from "../middlewares/errorMulter.js";
-
 
 const router = Router();
 
-router.post("/", upload.single("imagen"), errorMulter, crearProducto);
-
+router.route("/").post(upload.single("imagen"), errorMulter, crearProducto);
 
 export default router;
-
