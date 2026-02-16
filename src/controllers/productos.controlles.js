048335f60c679b4ccaa7a0276f06491338892e41
jonathan <jonathanfiorenza6@gmail.com>
2026-02-16 20:29:19 -0300

    corregi errores de duplicidad de carpetas en el controlador

--- src/controllers/productos.controlles.js
+++ src/controllers/productos.controlles.js
@@ -38,3 +38,33 @@ export const crearProducto = async (req, res) => {
   }
 };
 
+
+export const listarProductos = async (req, res) => {
+  try {
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar mostrar los productos" });
+  }
+};
+
+export const obtenerProductoPorId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const producto = await Producto.findById(id);
+    if (!producto) {
+      return res.status(404).json({ mensaje: "El producto no existe." });
+    } else {
+      res.status(200).json(producto);
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar obtener el producto." });
+  }
+};
+
